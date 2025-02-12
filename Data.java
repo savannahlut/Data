3f9fe596dba233e1579b03e8889abee62d3502e8
Savannah-Lu <savannahlut@nycstudents.net>
2025-02-12 02:52:55 +0000

    yipee

--- Data.java
+++ Data.java
@@ -1,6 +1,6 @@
 public class Data
 {
-    public static final int MAX = 30  /* value not shown */;
+    public static final int MAX = 30  /* value not shown, 30 test value */;
     private int[][] grid;
     private int x;
     private int y;
@@ -28,12 +28,22 @@ public class Data
     * grid has at least one element.
     */
     public int countIncreasingCols()
-    { /* to be implemented in part (b) */ }
+    {
+        int count = 0; 
+        for (int i = 0; i<x; i++){
+            boolean increasing = true;
+            for (int j = 1; j<y; j++){
+                if(grid[j-1][i] > grid[j][i])
+                    increasing = false;
+            } if(increasing) count++;
+        }
+        return count;
+    }
     // There may be instance variables, constructors, and methods that are not shown.
 
     public int[][] getGrid(){
-        for (int i = 0; i<x; i++){
-            for (int j = 0; j<y; j++){
+        for (int i = 0; i<y; i++){
+            for (int j = 0; j<x; j++){
             System.out.print(grid[i][j] + " ");
             } 
             System.out.println();
@@ -46,4 +56,12 @@ public class Data
         this.y = y;
         grid = new int[x][y];
     }
+
+    public Data(int[][] setter){
+        grid = setter; 
+        x = setter[0].length;
+        y = setter.length;
+    }
+
+    //i coulda used toString to print the grid but like i forgor...
 }
--- Main.java
+++ Main.java
@@ -4,5 +4,23 @@ public class Main {
         Data D = new Data(5, 5);
         D.repopulate();
         System.out.println(D.getGrid());
+
+        int[][] slay = 
+        {
+            {10, 50, 40},
+            {20, 40, 20},
+            {30, 50, 30}
+        };
+        Data girlboss = new Data(slay);
+        System.out.println(girlboss.getGrid());
+        System.out.println(girlboss.countIncreasingCols());
+
+        int[][] bjall = {
+            {10, 540, 440, 440},
+            {220, 450, 440, 190},
+        };
+        Data blanchin = new Data(bjall);
+        System.out.println(blanchin.getGrid());
+        System.out.println(blanchin.countIncreasingCols());
     }
 }
